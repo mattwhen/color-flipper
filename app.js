e51be9798a12a014eb1d39a91000d349d52874ac
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-02-25 12:48:47 -0600

    Added copy to clipboard feature

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const colors = ['green', 'red', 'rgba(133, 122, 200)', '#f15025'];
+const colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
+
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const clipboard = document.querySelector('.clipboard');
 
 btn.addEventListener('click', function () {
-	// Get random number between 0 and 3.
 	const randomNumber = getRandomNumber();
-	console.log(randomNumber);
-
-	// PROBLEM: When you click the button repeatedly, the randomNumber variable stays the same.
-	// Suggestion: IF the second number generated is the same as the previous, skip it.
 
 	/* Target the index from the array, based on the random 
     number that was generated. */
 	document.body.style.backgroundColor = colors[randomNumber];
 	color.innerHTML = colors[randomNumber];
+
+	clipboard.style.display = 'inline-block';
 });
 
 function getRandomNumber() {
 	return Math.floor(Math.random() * colors.length);
+}
+
+async function copyHex() {
+	try {
+		navigator.clipboard.writeText(color.textContent);
+	} catch (error) {
+		console.error('Error copying hex', error);
+	}
 }
--- hex.js
+++ hex.js
@@ -1,6 +1,7 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const clipboard = document.querySelector('.clipboard');
 
 btn.addEventListener('click', function () {
 	let hexColor = '#';
@@ -13,8 +14,18 @@ btn.addEventListener('click', function () {
 
 	document.body.style.backgroundColor = hexColor;
 	color.textContent = hexColor;
+	clipboard.style.display = 'inline-block';
+	console.log(color.value);
 });
 
 function getRandomNumber() {
 	return Math.floor(Math.random() * hex.length);
+}
+
+async function copyHex() {
+	try {
+		navigator.clipboard.writeText(color.textContent);
+	} catch (error) {
+		console.error('Error copying hex', error);
+	}
 }
